203a688d1b5711584aa9bf88c16a26a4de1ada37
Anton Belov <anton.belov.al7@gmail.com>
2024-11-25 11:25:56 +0000

    single_number solved

---
+++ src/single_number.rs
@@ -0,0 +1,9 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        nums.into_iter().fold(0, |acc, x| {
+            return acc^x;
+        })
+    }
+}
