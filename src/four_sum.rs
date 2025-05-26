c912cd932b291ff19887819bc55b00b91b0d2b3f
Anton Belov <anton.belov.al7@gmail.com>
2025-05-26 19:58:58 +0700

    four_sum solved

--- src/four_sum.rs
+++ src/four_sum.rs
@@ -1,126 +1,60 @@
-use std::collections::HashSet;
+use std::cmp::Ordering;
 
 pub struct Solution;
 
 impl Solution {
     pub fn four_sum(mut nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-
         let right_default = nums.len()-1;
-
-        if right_default < 3 {return vec![]};
-
-        let mut res: HashSet<Vec<i32>> = HashSet::new();
-
+        if right_default < 3 {
+            return vec![];
+        };
+        let mut res = Vec::new();
         nums.sort();
+        Solution::k_sum_recur(&nums, target, 4, &mut res, 0, &mut Vec::new());
 
-        for i in 0..nums.len()-3 {
-            let ith = nums[i];
+        res
+    }
 
-            if i > 0 && ith == nums[i - 1] {
-                continue;
-            }
-            
 
-            for j in i+1..nums.len()-2 {
+    fn k_sum_recur(nums: &Vec<i32>, target: i32, sum_num: usize, res: &mut Vec<Vec<i32>>, i: usize, arr: &mut Vec<i32>) {
+
+        if sum_num != 2 {
+            //* for loop calling the recur function
+            for j in i..nums.len()-(sum_num-1) {
+
+                //* eliminate duplicates
                 let jth = nums[j];
+                if j > i && jth == nums[j - 1] {
+                    continue;
+                }
 
-                let mut left = j+1;
-                let mut right = right_default;
+                arr.push(jth);
+                Solution::k_sum_recur(nums, target-jth, sum_num-1, res, j+1, arr);
+                arr.pop();
+            }
 
+        } else {
+            let mut left = i;
+            let mut right = nums.len()-1;
+
+            
+            while left < right {
                 
-                'wh: while left < right {
-                    
-                    let num_l = nums[left];
-                    let num_r = nums[right];
+                let num_l = nums[left];
+                let num_r = nums[right];
 
-                    let mut result: i32 = 0;
+                match (num_l + num_r).cmp(&target) {
+                    Ordering::Less => left += 1,
+                    Ordering::Greater => right -= 1,
+                    Ordering::Equal => {
+                        res.push(arr.iter().cloned().chain([num_l, num_r]).collect());
 
-                    for &number in &[ith, jth, num_l, num_r] {
-                        let (new_result, overflowed) = result.overflowing_add(number);
-                        if overflowed {
-                            right -= 1;
-                            continue 'wh;
+                        while (left < right) && (nums[left] == num_l) {
+                            left += 1;
                         }
-                        result = new_result;
-                    }
-
-                    match result {
-                        d if d < target => left += 1,
-                        d if d > target => right -= 1,
-                        d if d == target => {
-                            res.insert(vec![ith, jth, num_l, num_r]);
-
-                            while (left < right) && (nums[left] == num_l) {
-                                left += 1;
-                            }
-                            while (left < right) && (nums[right] == num_r) {
-                                right -= 1;
-                            }
-
-                        },
-                        _ => unreachable!()
                     }
                 }
             }
         }
-
-        res.into_iter().collect()
     }
-
-
-    // pub fn four_sum(mut nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-
-    //     if nums.len() < 4 {return vec![]};
-
-    //     let mut res: HashSet<Vec<i32>> = HashSet::new();
-
-    //     nums.sort();
-
-    //     for i in 0..nums.len()-3 {
-
-    //         if i > 0 && nums[i] == nums[i - 1] {
-    //             continue;
-    //         }
-            
-
-    //         for j in i+1..nums.len()-2 {
-
-    //             let mut left = j+1;
-    //             let mut right = nums.len()-1;
-
-                
-    //             'wh: while left < right {
-
-    //                 let mut result: i32 = 0;
-
-    //                 for &number in &[nums[i], nums[j], nums[left], nums[right]] {
-    //                     let (new_result, overflowed) = result.overflowing_add(number);
-    //                     if overflowed {
-    //                         right -= 1;
-    //                         continue 'wh;
-    //                     }
-    //                     result = new_result;
-    //                 }
-
-    //                 if result < target { 
-    //                     left += 1; 
-    //                 } else if result > target {
-    //                     right -= 1; 
-    //                 } else {
-                        
-    //                     res.insert(vec![nums[i], nums[j], nums[left], nums[right]]);
-
-    //                     while (left < right) && (nums[left] == nums[left]) {
-    //                         left += 1;
-    //                     }
-    //                     while (left < right) && (nums[right] == nums[right]) {
-    //                         right -= 1;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     res.into_iter().collect()
-    // }
 }
